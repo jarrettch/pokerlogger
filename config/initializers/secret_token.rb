a693fcd13b5520995e747984789addd49935f60b
jarrettch <jarrett.harris@gmail.com>
2013-10-18 17:26:40 -0700

    Yay new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pokerlogger::Application.config.secret_key_base = '7e98fbf23b723620631fc1572ea940194e05915e76bf9e66bf6ec95e92ca20620d4cdf21d053d4b0ebb9ddd95089e2908491a7758fbe0decadca21231f9cf3d4'
