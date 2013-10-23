4657d54768bb61249f327aa90c03b79ed1d9ce54
jarrettch <jarrett.harris@gmail.com>
2013-10-22 17:46:52 -0700

    Route, Model and Controller updated

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def index
+    @sessions = Session.all
+  end
+
+  def new
+    @session = Session.new
+  end
+
+  def show
+    @session = Session.find(params[:id])
+  end
+
+  def create
+    @session = Session.create(params[:session].permit(:date_played, :location, :game_type, :stakes, :hours_played, :profit_or_loss, :notes))
+    redirect_to :action => 'show', :id =>@team._id
+  end
+
+  def destroy
+    Session.find(params[:id]).destroy
+    redirect_to teams_url
+  end
+
+end
---
+++ app/models/session.rb
@@ -0,0 +1,13 @@
+class Session
+  include Mongoid::Document
+  
+  field :date_played, type: DateTime
+  field :location, type: String
+  field :game_type, type: String
+  field :stakes, type: String
+  field :hours_played, type: Float
+  field :profit_or_loss, type: Float
+  field :notes, type: String
+
+
+end
